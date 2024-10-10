8712206c852b1646696287497b16950538ca4e01
Griotold <gotjd9773@naver.com>
2024-10-10 17:19:23 +0900

    Lecture 07. 코틀린에서 예외를 다루는 방법

--- src/main/java/com/lannstark/lec07/Lec07Main.java
+++ src/main/java/com/lannstark/lec07/Lec07Main.java
@@ -4,7 +4,22 @@ import java.io.IOException;
 
 public class Lec07Main {
 
-  public static void main(String[] args) throws IOException {
-  }
+    public static void main(String[] args) throws IOException {
+    }
 
+    private int parseIntOrThrow(String str) {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(String.format("주어진 %s는 숫자가 아닙니다."));
+        }
+    }
+
+    private Integer parseIntOrThrowV2(String str) {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
 }
